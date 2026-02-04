dc99d244ca1d00b9871a5ce4e15c32c688b4bec8
Kakupro <chaitanyasethi3336@gmail.com>
2026-02-04 23:18:58 +0530

    Force trigger new deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 
 export default function Home() {
+  // Deployment trigger: 2026-02-04
   return (
     <main className="relative w-full min-h-screen">
       <ThreeBackground />
